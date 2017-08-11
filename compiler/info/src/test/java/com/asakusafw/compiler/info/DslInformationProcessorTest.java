e8061300c755147fe44123bbb238014fe18f832e
Suguru ARAKAWA <public@ashigeru.com>
2017-08-11 09:47:12 +0900

    Enable limited `batch-info.json`.
    `batch-info.json` is also now limited available for Asakusa on MapReduce.
    It only includes following information:
    * batch parameter (`list parameter`)
    * jobflow graph (`{list,draw} jobflow`)
    * task graph (`{list,draw} jobflow` with `-v`)

---
+++ compiler/info/src/test/java/com/asakusafw/compiler/info/DslInformationProcessorTest.java
@@ -0,0 +1,160 @@
+/**
+ * Copyright 2011-2017 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.compiler.info;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.asakusafw.compiler.flow.FlowCompilerOptions;
+import com.asakusafw.compiler.flow.Location;
+import com.asakusafw.compiler.testing.DirectBatchCompiler;
+import com.asakusafw.compiler.yaess.testing.batch.ComplexBatch;
+import com.asakusafw.compiler.yaess.testing.batch.DiamondBatch;
+import com.asakusafw.compiler.yaess.testing.batch.SimpleBatch;
+import com.asakusafw.info.BatchInfo;
+import com.asakusafw.info.JobflowInfo;
+import com.asakusafw.info.task.TaskInfo;
+import com.asakusafw.info.task.TaskListAttribute;
+import com.asakusafw.vocabulary.batch.BatchDescription;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Test for {@link DslInformationProcessor}.
+ */
+public class DslInformationProcessorTest {
+
+    /**
+     * Temporary folder.
+     */
+    @Rule
+    public final TemporaryFolder folder = new TemporaryFolder();
+
+    /**
+     * simple case.
+     */
+    @Test
+    public void simple() {
+        BatchInfo info = compile(SimpleBatch.class);
+
+        assertThat(info.getId(), is("simple"));
+        assertThat(info.getJobflows(), hasSize(1));
+
+        JobflowInfo f0 = jobflow(info, "first");
+        assertThat(f0.getBlockerIds(), hasSize(0));
+
+        assertThat(tasks(f0, TaskInfo.Phase.INITIALIZE), hasSize(1));
+        assertThat(tasks(f0, TaskInfo.Phase.IMPORT), hasSize(1));
+        assertThat(tasks(f0, TaskInfo.Phase.PROLOGUE), hasSize(1));
+        assertThat(tasks(f0, TaskInfo.Phase.MAIN), hasSize(1));
+        assertThat(tasks(f0, TaskInfo.Phase.EPILOGUE), hasSize(1));
+        assertThat(tasks(f0, TaskInfo.Phase.EXPORT), hasSize(1));
+        assertThat(tasks(f0, TaskInfo.Phase.FINALIZE), hasSize(1));
+    }
+
+    /**
+     * w/ multiple stages.
+     */
+    @Test
+    public void stages() {
+        BatchInfo info = compile(ComplexBatch.class);
+
+        assertThat(info.getId(), is("complex"));
+        assertThat(info.getJobflows(), hasSize(1));
+
+        JobflowInfo f0 = jobflow(info, "last");
+        List<TaskInfo> main = tasks(f0, TaskInfo.Phase.MAIN);
+
+        assertThat(main, hasSize(3));
+
+        TaskInfo s0 = main.stream()
+            .filter(it -> it.getBlockers().isEmpty())
+            .findFirst()
+            .get();
+
+        TaskInfo s1 = main.stream()
+                .filter(it -> it.getBlockers().contains(s0.getId()))
+                .findFirst()
+                .get();
+
+        main.stream()
+                .filter(it -> it.getBlockers().contains(s1.getId()))
+                .findFirst()
+                .get();
+    }
+
+    /**
+     * w/ multiple flow.
+     */
+    @Test
+    public void flows() {
+        BatchInfo info = compile(DiamondBatch.class);
+
+        assertThat(info.getId(), is("diamond"));
+        assertThat(info.getJobflows(), hasSize(4));
+
+        JobflowInfo f0 = jobflow(info, "first");
+        JobflowInfo f1 = jobflow(info, "left");
+        JobflowInfo f2 = jobflow(info, "right");
+        JobflowInfo f3 = jobflow(info, "last");
+
+        assertThat(f0.getBlockerIds(), hasSize(0));
+        assertThat(f1.getBlockerIds(), containsInAnyOrder(f0.getId()));
+        assertThat(f2.getBlockerIds(), containsInAnyOrder(f0.getId()));
+        assertThat(f3.getBlockerIds(), containsInAnyOrder(f1.getId(), f2.getId()));
+    }
+
+    private static JobflowInfo jobflow(BatchInfo parent, String id) {
+        return parent.getJobflows().stream()
+                .filter(it -> it.getId().equals(id))
+                .findFirst()
+                .get();
+    }
+
+    private List<TaskInfo> tasks(JobflowInfo jobflow, TaskInfo.Phase phase) {
+        return jobflow.findAttribute(TaskListAttribute.class)
+                .map(it -> it.getPhases().getOrDefault(phase, Collections.emptyList()))
+                .orElse(Collections.emptyList());
+    }
+
+    private BatchInfo compile(Class<? extends BatchDescription> batchClass) {
+        try {
+            File output = folder.newFolder("output");
+            DirectBatchCompiler.compile(
+                    batchClass,
+                    "com.example",
+                    Location.fromPath("testing", '/'),
+                    output,
+                    folder.newFolder("working"),
+                    Collections.emptyList(),
+                    getClass().getClassLoader(),
+                    new FlowCompilerOptions());
+            File script = new File(output, DslInformationProcessor.PATH);
+            return new ObjectMapper().readValue(script, BatchInfo.class);
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+}
---
+++ integration/src/integration-test/java/com/asakusafw/integration/mapreduce/PortalTest.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2011-2017 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.integration.mapreduce;
+
+import static com.asakusafw.integration.mapreduce.Util.*;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import com.asakusafw.integration.AsakusaConfigurator;
+import com.asakusafw.integration.AsakusaConstants;
+import com.asakusafw.integration.AsakusaProjectProvider;
+import com.asakusafw.utils.gradle.Bundle;
+import com.asakusafw.utils.gradle.ContentsConfigurator;
+
+/**
+ * Test for the portal command.
+ */
+public class PortalTest {
+
+    /**
+     * project provider.
+     */
+    @ClassRule
+    public static final AsakusaProjectProvider PROVIDER = new AsakusaProjectProvider()
+            .withProject(ContentsConfigurator.copy(data("mapreduce")))
+            .withProject(ContentsConfigurator.copy(data("ksv")))
+            .withProject(ContentsConfigurator.copy(data("logback-test")))
+            .withProject(AsakusaConfigurator.projectHome())
+            .withProvider(provider -> {
+                // install framework only once
+                framework = provider.newInstance("inf")
+                        .gradle("attachMapreduceBatchapps", "installAsakusafw")
+                        .getFramework();
+            });
+
+    static Bundle framework;
+
+    /**
+     * {@code list parameter}.
+     */
+    @Test
+    public void list_parameter() {
+        framework.withLaunch(AsakusaConstants.CMD_PORTAL, "list", "parameter", "-v",
+                "perf.average.sort");
+    }
+
+    /**
+     * {@code list jobflow}.
+     */
+    @Test
+    public void list_jobflow() {
+        framework.withLaunch(AsakusaConstants.CMD_PORTAL, "list", "jobflow", "-v",
+                "perf.average.sort");
+    }
+
+    /**
+     * {@code draw jobflow}.
+     */
+    @Test
+    public void draw_jobflow() {
+        framework.withLaunch(AsakusaConstants.CMD_PORTAL, "draw", "jobflow", "-v",
+                "perf.average.sort");
+    }
+}
